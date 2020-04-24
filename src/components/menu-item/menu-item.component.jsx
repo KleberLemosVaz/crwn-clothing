42724d41b10f38840cc56108389ef37214b8e6de
KleberLemosVaz <kleberlvaz@gmail.com>
2020-04-24 12:30:49 -0300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -13,7 +13,7 @@ const MenuItem = ({title,imageUrl, size, history,linkUrl, match}) => (
         />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
-            <san className="title">SHOP NOW</san>
+            <span className="title">SHOP NOW</span>
         </div>
     </div>
 )
