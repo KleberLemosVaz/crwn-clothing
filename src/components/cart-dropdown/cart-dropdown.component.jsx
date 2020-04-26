6730d2a92d0a0b5a8178221bb44f844650d960b3
KleberLemosVaz <kleberlvaz@gmail.com>
2020-04-26 09:04:40 -0300

    added checkout page, chechout item component and update cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { withRouter } from 'react-router-dom'
 
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems,history, dispatch}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
+                cartItems.length ? (
                 cartItems.map(cartItem => (
                      <CartItem key={cartItem.id} item={cartItem}/>
-                ))}
+                )) 
+                ) : (
+                <span className="empty-message">Your cart is empty</span>   
+                )}
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout')
+            dispatch(toggleCartHidden())
+            }}>
+                GO TO CHECKOUT
+        </CustomButton>
     </div>
 )
 
-const mapSatateToProps = (state) => ({
-    cartItems: selectCartItems(state)
+const mapSatateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 })
 
-export default connect(mapSatateToProps)(CartDropdown)
+export default withRouter(connect(mapSatateToProps)(CartDropdown))
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 import './custom-button.styles.scss'
 
 const CustomButton = ( {children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button className={`${inverted ? 'inverted':""} custom-button`} {...otherProps}>
-        {children}
-    </button>
+    <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 )
 
 export default CustomButton
